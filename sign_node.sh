25170f4336ab21be9264c909c5148f3c4fc87a97
LMacchi <lau.macchi@gmail.com>
2017-08-16 09:58:24 -0500

    Add CA scripts

---
+++ sign_node.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+  echo "Error: Only one argument accepted"
+  echo "Usage: $0 laura.puppetlabs.vm"
+  exit 2
+fi
+
+NODE=$1
+
+CERT=$(puppet config print hostcert)
+KEY=$(puppet config print hostprivkey) 
+CACERT=$(puppet config print localcacert) 
+MASTER=$(puppet config print certname)
+
+curl -s -X PUT --data @/vagrant/scripts/sign_node.json --cert $CERT --key $KEY --cacert $CACERT https://${MASTER}:8140/puppet-ca/v1/certificate_status/${NODE}?environment=production
